0b9e402b92e079603d587856a41869857a1618f9
Globateur <45522552+Globateur@users.noreply.github.com>
2020-02-04 18:59:54 +0100

    Add files via upload

---
+++ FirstPersonCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    public float sensivity = 100f;
+
+    float xrotation = 0f;
+    public Transform character;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensivity * Time.deltaTime;
+
+        xrotation -= mouseY;
+        xrotation = Mathf.Clamp(xrotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xrotation, 0f, 0f);
+        character.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ FirstPersonController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[RequireComponent(typeof(Rigidbody))]
+public class FirstPersonController : MonoBehaviour
+{
+    public float speed = 1f;
+    public float StepInterval = 5f;
+    public float detente = 5f;
+
+    
+    public bool isGrounded;
+    Rigidbody rb_;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb_ = GetComponent<Rigidbody>();
+        isGrounded = true;
+        // Met le curseur au milieu de l'écran
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb_.freezeRotation = true;
+        // Mouvement Vertical / Horizontal
+        float x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+
+
+        transform.Translate(x, 0, z);
+
+
+        // Saut
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+
+            rb_.AddForce(new Vector3(0, detente, 0), ForceMode.Impulse);
+            isGrounded = false;
+            
+        }
+    }
+    void OnCollisionEnter()
+    {
+        isGrounded = true;
+    }
+ 
+
+}
